264e641a0fadf54571020af349404878e43d324b
hasibzunair <hasibzunair@gmail.com>
2024-08-20 14:37:32 -0400

    add octopus image

--- demo.py
+++ demo.py
@@ -43,7 +43,7 @@ if __name__ == "__main__":
     parser.add_argument(
         "--img-path",
         type=str,
-        default="data/examples/dinosaur.jpeg",
+        default="data/examples/octopus.jpeg",
         help="Image path.",
     )
     parser.add_argument(
